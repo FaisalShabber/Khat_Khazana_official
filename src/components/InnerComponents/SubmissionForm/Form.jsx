09109ad3b656b6dc944584ec9f474df074797645
Faisal Shabber <83510525+FaisalShabber@users.noreply.github.com>
2025-09-14 14:59:17 +0500

    Form Filed Updated

--- src/components/InnerComponents/SubmissionForm/Form.jsx
+++ src/components/InnerComponents/SubmissionForm/Form.jsx
@@ -7,12 +7,13 @@ import FileInput from "./FileInput";
 import DropdownField from "./DropdownField";
 
 const Form = () => {
+  // States
   const [uploadType, setUploadType] = useState("Both");
   const [before2000, setBefore2000] = useState("No");
   const [letterNarrativeFormat, setLetterNarrativeFormat] = useState("text");
   const [photoNarrativeFormat, setPhotoNarrativeFormat] = useState("text");
-  const [hasReadGuidelines, setHasReadGuidelines] = useState(false);
-  const [agreedTermsSubmission, setAgreedTermsSubmission] = useState(false);
+  const [hasReadGuidelines, setHasReadGuidelines] = useState(true);
+  const [agreedTermsSubmission, setAgreedTermsSubmission] = useState(true);
   const [letterLanguage, setLetterLanguage] = useState("");
   const [letterCategory, setLetterCategory] = useState("");
   const [decade, setDecade] = useState("");
@@ -26,6 +27,7 @@ const Form = () => {
     console.log("Form Data:", data);
     alert("Form submitted successfully!");
     e.target.reset();
+
     // reset states
     setUploadType("Both");
     setBefore2000("No");
@@ -105,6 +107,12 @@ const Form = () => {
               <>
                 <FormSection title="Letter Information">
                   <div className="grid grid-cols-1 md:grid-cols-3 gap-x-8 gap-y-6">
+                    <InputField
+                      label="Title"
+                      name="Title"
+                      type="text"
+                      required
+                    />
                     <DropdownField
                       label="Category"
                       name="letterCategory"
@@ -112,9 +120,15 @@ const Form = () => {
                       value={letterCategory}
                       onChange={(e) => setLetterCategory(e.target.value)}
                       options={[
-                        { value: "Love Letter", label: "Love Letter" },
-                        { value: "War", label: "War" },
-                        { value: "Political", label: "Political" },
+                        { value: "Family", label: "Family" },
+                        { value: "love Letter", label: "love Letter" },
+                        { value: "War Political", label: "War Political" },
+                        { value: "Travel", label: "Travel" },
+                        { value: "Diary Pages", label: "Diary Pages" },
+                        { value: "PostCards", label: "PostCards" },
+                        { value: "Calenders", label: "Calenders" },
+                        { value: "MoviePosters", label: "MoviePosters" },
+                        { value: "Others", label: "Others" },
                       ]}
                     />
                     <DropdownField
@@ -147,12 +161,14 @@ const Form = () => {
                   </div>
                 </FormSection>
 
+                {/* Letter Transcript */}
                 <FormSection>
                   <div className="flex flex-col md:flex-row justify-between gap-3">
-                    <div className="flex flex-col md:w-1/2 w-full">
+                    <div className="flex flex-col justify-between w-full">
                       <div className="flex justify-between">
                         <label className="font-bold text-sm block">
-                          Letter Transcript <span className="text-red-600">*</span>
+                          Letter Transcript{" "}
+                          <span className="text-red-600">*</span>
                         </label>
                         <span className="flex gap-4 text-sm">
                           <label className="flex items-center gap-2">
@@ -175,31 +191,48 @@ const Form = () => {
                             />
                             Audio
                           </label>
+                          <label className="flex items-center gap-2">
+                            <input
+                              type="radio"
+                              name="letterNarrativeFormat"
+                              value="both"
+                              checked={letterNarrativeFormat === "both"}
+                              onChange={() => setLetterNarrativeFormat("both")}
+                            />
+                            Both
+                          </label>
                         </span>
                       </div>
 
-                      {letterNarrativeFormat === "text" ? (
-                        <InputField
-                          wrapperClassName="w-full "
-                          className="h-24"
-                          name="letterNarrative"
-                        />
-                      ) : (
-                        <FileInput
-                          name="letterAudioFile"
-                          subtext="Only MP3 Format (Max 5MB)"
-                          previewType="audio"
-                          className="mt-5"
-                        />
-                      )}
+                      <div className="flex flex-col lg:flex-row justify-between gap-10">
+                        {(letterNarrativeFormat === "text" ||
+                          letterNarrativeFormat === "both") && (
+                          <InputField
+                            wrapperClassName="w-full mt-4"
+                            className="h-24"
+                            name="letterNarrative"
+                          />
+                        )}
+
+                        {(letterNarrativeFormat === "audio" ||
+                          letterNarrativeFormat === "both") && (
+                          <FileInput
+                            name="letterAudioFile"
+                            subtext="Only MP3 Format (Max 5MB)"
+                            previewType="audio"
+                            className="mt-5"
+                          />
+                        )}
+                      </div>
                     </div>
-                    <InputField
-                      wrapperClassName="w-full md:w-1/2 "
-                      className="h-24"
-                      label="Narrative (Optional)"
-                      name="letterNarrativeOptional"
-                    />
                   </div>
+
+                  <InputField
+                    wrapperClassName="w-full mt-6"
+                    className="h-24"
+                    label="Narrative (Optional)"
+                    name="letterNarrativeOptional"
+                  />
                 </FormSection>
               </>
             )}
@@ -228,12 +261,14 @@ const Form = () => {
                   </div>
                 </FormSection>
 
+                {/* Photo Transcript */}
                 <FormSection>
                   <div className="flex flex-col md:flex-row justify-between gap-3">
-                    <div className="flex flex-col md:w-1/2 w-full">
+                    <div className="flex flex-col justify-between w-full">
                       <div className="flex justify-between">
                         <label className="font-bold text-sm block">
-                          Photo Transcript <span className="text-red-600">*</span>
+                          Photographs Transcript{" "}
+                          <span className="text-red-600">*</span>
                         </label>
                         <span className="flex gap-4 text-sm">
                           <label className="flex items-center gap-2">
@@ -256,31 +291,48 @@ const Form = () => {
                             />
                             Audio
                           </label>
+                          <label className="flex items-center gap-2">
+                            <input
+                              type="radio"
+                              name="photoNarrativeFormat"
+                              value="both"
+                              checked={photoNarrativeFormat === "both"}
+                              onChange={() => setPhotoNarrativeFormat("both")}
+                            />
+                            Both
+                          </label>
                         </span>
                       </div>
 
-                      {photoNarrativeFormat === "text" ? (
-                        <InputField
-                          wrapperClassName="w-full "
-                          className="h-24"
-                          name="photoNarrative"
-                        />
-                      ) : (
-                        <FileInput
-                          name="photoAudioFile"
-                          subtext="Only MP3 Format (Max 5MB)"
-                          previewType="audio"
-                          className="mt-5"
-                        />
-                      )}
+                      <div className="flex flex-col lg:flex-row justify-between gap-10">
+                        {(photoNarrativeFormat === "text" ||
+                          photoNarrativeFormat === "both") && (
+                          <InputField
+                            wrapperClassName="w-full mt-4"
+                            className="h-24"
+                            name="photoNarrative"
+                          />
+                        )}
+
+                        {(photoNarrativeFormat === "audio" ||
+                          photoNarrativeFormat === "both") && (
+                          <FileInput
+                            name="photoAudioFile"
+                            subtext="Only MP3 Format (Max 5MB)"
+                            previewType="audio"
+                            className="mt-5"
+                          />
+                        )}
+                      </div>
                     </div>
-                    <InputField
-                      wrapperClassName="w-full md:w-1/2 "
-                      className="h-24"
-                      label="Narrative (Optional)"
-                      name="photoNarrativeOptional"
-                    />
                   </div>
+
+                  <InputField
+                    wrapperClassName="w-full mt-6"
+                    className="h-24"
+                    label="Narrative (Optional)"
+                    name="photoNarrativeOptional"
+                  />
                 </FormSection>
               </>
             )}
